67321c149f8579c53dc6736f1eb3973d2146ac31
vin4math <vin4math@gmail.com>
2021-03-26 23:29:52 +0530

    header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './components/Header/header'
+import './styles.css'
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
